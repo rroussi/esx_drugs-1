937f4512cf27cf3e248440c45fa7ed5df8a3c019
rroussi <64332351+rroussi@users.noreply.github.com>
2020-04-26 00:21:02 +0300

    Added Finnish translations!

---
+++ locales/fi.lua
@@ -0,0 +1,30 @@
+Locales ['fi'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Paina ~INPUT_CONTEXT~ Korjataksesi ~g~Cannabis~s~ sato.',
+  ['weed_inventoryfull'] = 'Sinulla ei ole tilaa repussasi ~g~Cannabis~s~.',
+  ['weed_processprompt'] = 'Paina ~INPUT_CONTEXT~ Aloittaaksesi ~g~Process Cannabis~s~.',
+  ['weed_processingstarted'] = 'Valmistetaan ~g~Cannabis~s~ Muotoon ~g~Marijuana~s~...',
+  ['weed_processingfull'] = 'Valmistus lopetettu ~r~canceled~s~ Reppusi on täynnä!',
+  ['weed_processingenough'] = 'Sinulla pitää olla ~b~3x~s~ ~g~Cannabis~s~ Valmistaaksesi',
+  ['weed_processed'] = 'Olet valmistanut ~b~3x~s~ ~g~Cannabis~s~ Muotoon ~b~1x~s~ ~g~Marijuana~s~',
+  ['weed_processingtoofar'] = 'Valmistus lopetettu ~r~canceled~s~ Olet mennyt liian kauaksi alueelta',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Paina ~INPUT_CONTEXT~ Puhuaksesi ~r~Drug Dealer~s~.',
+  ['dealer_title'] = 'Huumeiden välittäjä',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Sinulla ei ole tarpeeksi myytävää kamaa!',
+  ['dealer_sold'] = 'Olet myynyt ~b~%sx~s~ ~y~%s~s~ Hintaan ~g~$%s~s~',
+
+  -- license
+  ['license_title'] = 'Sinulla pitää olla lisenssi tehdäksesi tätä, sen voit ostaa täältä!',
+  ['license_no'] = 'Ei',
+  ['license_bought'] = 'Ostit ~b~%s~s~ Hintaan ~r~$%s~s~',
+  ['license_bought_fail'] = 'Ei sinulla ole varaa lisenssiin! ~b~%s~s~!',
+  ['license_weed'] = 'Kannabiksen valmistus lisenssi,
+
+  -- blips
+  ['blip_weedfield'] = 'Kannabis Pelto',
+  ['blip_weedprocessing'] = 'Kannabiksen Valmistus',
+  ['blip_drugdealer'] = 'Huumeiden Välittäjä',
+}
